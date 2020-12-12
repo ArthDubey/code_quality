4d29c9d528c08661a62582b89aed34f913fdef14
Arth Dubey <arthdubey07@gmail.com>
2020-12-12 14:47:23 +0530

    main.py completed; arguments added;

--- code_quality/__main__.py
+++ code_quality/__main__.py
@@ -1,3 +1,19 @@
 from .directory_score_aggr import aggregateScore
+import os
+import argparse
 
-aggregateScore(input())
+
+parser = argparse.ArgumentParser(
+    description="""
+                    A light-weight library for testing code quality. Includes various software principles check.
+                """
+)
+
+DEFAULT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+parser.add_argument("-d", "--Directory",
+                    help="The filepath for repository or the file to execute checks on.", type=str, default=DEFAULT_DIR)
+
+args = parser.parse_args()
+
+aggregateScore(args.Directory)
